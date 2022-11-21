8a06bdb75048db922260e820c0f2e8d3a868665d
Tyler Faulkner <tylersfaulkner@gmail.com>
2022-11-21 15:30:05 -0600

    Created Top Navigation Bar

---
+++ pages/topbar.js
@@ -0,0 +1,13 @@
+export default function TopBar() {
+    return (
+        <div className="topnav">
+            <text>tyler faulkner</text>
+            <div className="elements">
+                <a className="topelement">Home</a>
+                <a className="topelement">Projects</a>
+                <a className="topelement">Tutorials</a>
+                <a className="topelement">Contact</a>
+            </div>
+        </div>
+    );
+}
