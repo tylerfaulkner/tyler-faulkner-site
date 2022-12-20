b4a3bc8f2b1971f8f44eaab0f63fc68d47763e05
tylerfaulkner <tylersfaulkner@gmail.com>
2022-12-19 20:44:26 -0600

    Fixed Menu Bar on Mobile

--- components/topbar.js
+++ components/topbar.js
@@ -6,17 +6,31 @@ import { useMediaQuery } from "react-responsive";
 export default function TopBar() {
     const [isMobile, setIsMobile] = useState(null);
     const [currentPage, setCurrentPage] = useState('/');
+    const [showMenu, setShowMenu] = useState(false);
     useEffect(()=> {
         setIsMobile(window.innerWidth < 600);
         setCurrentPage(window.location.pathname);
         window.addEventListener('resize', ()=>{setIsMobile(window.innerWidth < 600)})
+        window.addEventListener('click', (event)=>{
+            if(event.target.id != 'show nav'){
+                setShowMenu(false)
+            }
+        })
+        window.addEventListener('scroll', ()=>{
+            setShowMenu(false);
+        })
     })
     const router = useRouter();
 
     function onChange(selector){
         console.log(selector)
-        router.push(selector.target.value);
+        router.push(selector.target.id);
     }
+
+    function click(){
+        setShowMenu(true);
+    }
+
     return (
         <div className="topnav">
             <text className="text-black">tyler faulkner</text>
@@ -26,12 +40,37 @@ export default function TopBar() {
                 <Link href={"/tutorials"} className="topelement">Tutorials</Link>
                 <Link href={"/contact"} className="topelement">Contact</Link>
             </div>  : 
-            <select defaultValue={currentPage} onChange={onChange} className='menu-selector'>               
-              <option value="/">Home</option> 
-              <option value="/projects">Projects</option> 
-              <option value="/tutorials">Tutorials</option> 
-              <option value="/contact">Contact</option> 
-            </select> 
+            <a onClick={click}>
+                <span id='show nav' className="material-symbols-outlined mr">
+                menu
+                </span>
+            </a>
+            }
+            {showMenu &&
+            <table className="nav-menu">
+                <tbody>
+                <tr onClick={onChange}>
+                    <td id='/'>
+                        Home
+                    </td>
+                </tr>
+                <tr onClick={onChange}>
+                    <td id='/projects'>
+                        Projects
+                    </td>
+                </tr>
+                <tr onClick={onChange}>
+                    <td id='/tutorials'>
+                        Tutorials
+                    </td>
+                </tr>
+                <tr onClick={onChange}>
+                    <td id='/contact'>
+                        Contact
+                    </td>
+                </tr>
+                </tbody>
+            </table>
             }
         </div>
     );
